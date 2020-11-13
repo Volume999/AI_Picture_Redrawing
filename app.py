15e75e39559597024ef7324592ecd87fb6547d71
Volume999 <Cholponbaev_a@auca.kg>
2020-11-13 19:50:25 +0600

    dynamic images from database

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, escape, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from PIL import Image
 from resizeimage import resizeimage
 import os
-import Database
-
+import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+app.secret_key = b'Alibek'
+
+cards_per_page = 6
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userId = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(30), nullable=False)
     password = db.Column(db.String(30), nullable=False)
 
@@ -22,29 +24,29 @@ class User(db.Model):
 
 
 class Photobook(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(20), nullable=False)
+    photobookId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    photobookName = db.Column(db.String(20), nullable=False)
     description = db.Column(db.String(100), nullable=True)
     ownerId = db.Column(db.Integer, nullable=False)
     isPrivate = db.Column(db.Boolean, default=False)
-    dateCreated = db.Column(db.Date)
+    dateCreated = db.Column(db.Date, default=datetime.datetime.utcnow())
     isDeleted = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
-        return f'Photobook {self.name}'
+        return f'Photobook {self.photobookName}'
 
 
 class Photo(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(1000), nullable=False)
+    photoId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    photoName = db.Column(db.String(1000), nullable=False)
     photobookId = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return f'Photo {self.name}'
+        return f'Photo {self.photoName}'
 
 
 class UserFollowsBooks(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    ufbId = db.Column(db.Integer, primary_key=True, autoincrement=True)
     userId = db.Column(db.Integer, nullable=False)
     photobookId = db.Column(db.Integer, nullable=False)
 
@@ -52,14 +54,37 @@ class UserFollowsBooks(db.Model):
         return f'User {self.userId} follow {self.photobookId}'
 
 
+def reinitialize(db):
+    db.drop_all()
+    db.create_all()
+    us = User(username='admin', password='123')
+    db.session.add(us)
+    db.session.commit()
+
+
+@app.route('/logout', methods=['POST', 'GET'])
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    books = Photobook.query.limit(cards_per_page).all()
+    def getFirstPhoto(photobookId):
+        return Photo.query.join(Photobook, Photobook.photobookId == Photo.photobookId).filter(
+        Photobook.photobookId == photobookId).first()
+    firstPhoto = {b.photobookId: getFirstPhoto(b.photobookId).photoName if getFirstPhoto(b.photobookId) is not None else 'default.jpg' for b in books}
+    print(firstPhoto)
+    return render_template('index.html', books=books, firstPhoto=firstPhoto)
 
 
-@app.route('/view_book')
-def view_book():
-    return render_template('view_book.html')
+@app.route('/view_book/<int:id>')
+def view_book(id):
+    photos = Photo.query.filter_by(photobookId=id).limit(cards_per_page)
+    photobook = Photobook.query.filter_by(photobookId=id).first()
+    author = User.query.filter_by(userId=photobook.ownerId).first()
+    return render_template('view_book.html', photos=photos, photobook=photobook, author=author)
 
 
 photos = UploadSet('photos', IMAGES)
@@ -73,14 +98,28 @@ configure_uploads(app, photos)
 def upload():
     if request.method == 'POST':
         photobook_name = request.form['book_name']
-        description = request.form['desc']
+        desc = request.form['desc']
         # Photos
         files = request.files.getlist("photo")
         filename = []
-        for file in files:
-            fn = photos.save(file)
-            filename.append(fn)
-        return ' '.join([photobook_name, description, ' '.join(filename)])
+        pb = Photobook(photobookName=photobook_name,
+                       description=desc,
+                       ownerId=session['id'])
+        db.session.add(pb)
+        db.session.commit()
+        if any(f for f in files):
+            for file in files:
+                fn = photos.save(file)
+                print(fn)
+                filename.append(fn)
+                photo = Photo(
+                    photoName=fn, photobookId=pb.photobookId
+                )
+                db.session.add(photo)
+        db.session.commit()
+        flash('Your photobook has been added!')
+        return redirect(url_for('index'))
+        # return ' '.join([photobook_name, description, ' '.join(filename)])
     return render_template('upload.html')
 
 
@@ -89,8 +128,16 @@ def login():
     if request.method == 'POST':
         username, password = request.form['username'], request.form['password']
         query = User.query.filter_by(username=username).filter_by(password=password).all()
-        return 'Log-on' if len(query) != 0 else 'Incorrect credentials ' + username + ' ' + password
+        # return 'Log-on' if len(query) != 0 else 'Incorrect credentials ' + username + ' ' + password
+        if len(query) == 1:
+            session['username'] = username
+            session['id'] = query[0].userId
+            flash(f'Welcome, {username}')
+            return redirect(url_for('index'))
+        else:
+            return 'There has been a problem with your login (incorrect credentials, broken database)'
     return render_template('login.html')
+
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -101,6 +148,7 @@ def register():
         db.session.commit()
         return redirect(url_for('login'))
     return render_template('register.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
